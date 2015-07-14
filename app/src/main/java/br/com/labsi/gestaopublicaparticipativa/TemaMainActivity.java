b1ce69e2adc4aaef9e94ac732a13cf4e2ded30fa
marcelobazevedo <marcelobazevedo@gmail.com>
2015-07-14 18:59:10 -0300

    versão corrigida

---
+++ app/src/main/java/br/com/labsi/gestaopublicaparticipativa/TemaMainActivity.java
@@ -0,0 +1,18 @@
+package br.com.labsi.gestaopublicaparticipativa;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marcelo on 25/01/2015.
+ */
+public class TemaMainActivity extends AppCompatActivity {
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activitu_main_subtema);
+
+        }
+    }
